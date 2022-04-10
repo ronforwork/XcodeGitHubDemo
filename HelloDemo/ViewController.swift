fc5deb140b5645e8c2960f798a6a56e5ab1bed4f
Ron <ronforwork@gmail.com>
2022-04-10 21:49:40 +0800

    完成Hello Page

--- HelloDemo/ViewController.swift
+++ HelloDemo/ViewController.swift
@@ -8,13 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Hello")
     }
 
-
+    @IBAction func clickSend(_ sender: Any) {
+        let name = tfName.text ?? ""
+        let text = "Hello, \(name)"
+        lbResult.text = text
+    }
+    
 }
 
